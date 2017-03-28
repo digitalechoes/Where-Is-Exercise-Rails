346e92585d20b41d5322601377e13f3df5c42a71
digitalecho <writekatemoc@gmail.com>
2017-03-28 17:34:06 -0500

    Added back old changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,19 @@ class MainController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :incoming
 
   def index
-  	if params[:query]
-  		@response = [params[:query], ConferenceRoom.reply_for(params[:query])]
+    query = params[:query]
+
+    p query.nil?
+    p "??????"
+    render "index" && return if query.nil?
+
+    p "Dang it"
+    render_index("No query has been entered") && return if query.blank?
+
+    if query.downcase.eql?("list conference rooms")
+      render_index(ConferenceRoom.list_rooms)
+    else
+      render_index(ConferenceRoom.reply_for(query))
     end
   end
 
@@ -18,5 +29,11 @@ class MainController < ApplicationController
       body: response
       )
     render nothing: true
+  end
+
+  private
+
+  def render_index(response)
+    render locals: {query: params[:query], result: response}
   end
 end
